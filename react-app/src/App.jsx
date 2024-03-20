b820fa079eb483fd1196f999d94fe1ae8fc9e8a6
MaleneRJohansen <malene9@hotmail.no>
2024-03-20 11:22:35 +0100

    Lagt inn routing

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,9 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+import Nav from './components/Nav'
+import { Routes, Route } from "react-router-dom"
 
 function App() {
 
-  return
+  return (
+    <Routes>
+      <Route path='/' element={<Nav />}/>
+      <Route path='/css' element={<Nav />}/>
+      <Route path='/javascript' element={<Nav />}/>
+      <Route path='/react' element={<Nav />}/>
+      <Route path='/sanity' element={<Nav />}/>
+    </Routes>
+  )
 }
 
 export default App
